a02b45a3d3f2f2c2a78a00991bf48ae3f1be10b1
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-03 17:06:06 +0600

    tailwind css config containder

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return <div className="container bg-green-500">Home</div>;
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      // default breakpoints but with 40px removed
+      screens: {
+        "2xl": "1600px",
+      },
+    },
     extend: {},
   },
   plugins: [],
