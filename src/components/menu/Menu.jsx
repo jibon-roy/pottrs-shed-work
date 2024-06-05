d9c36d569e881b712828149166f8cd5cde648b88
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-05 18:28:19 +0600

    select updated

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -13,28 +13,28 @@ const MenuSection = () => {
       <div className="font-Inter my-1 mx-auto max-w-xl justify-center items-center flex-wrap flex gap-4">
         <Link to="#">New Arrivals</Link>
         <Link to="#">Windspinners</Link>
-        <select className="">
+        <select name="select_option1" id="select_option1" className="">
           <option>Ceramics/Pots</option>
           <option>Option 1</option>
           <option>Option 2</option>
           <option>Option 3</option>
           <option>Option 4</option>
         </select>
-        <select className="">
+        <select name="select_option2" id="select_option2" className="">
           <option>Metal Animal/Birds</option>
           <option>Option 1</option>
           <option>Option 2</option>
           <option>Option 3</option>
           <option>Option 4</option>
         </select>
-        <select className="">
+        <select name="select_option3" id="select_option3" className="">
           <option>Rusty Gardenware</option>
           <option>Option 1</option>
           <option>Option 2</option>
           <option>Option 3</option>
           <option>Option 4</option>
         </select>
-        <select className="">
+        <select name="select_option4" id="select_option4" className="">
           <option>Decorative/Ornaments</option>
           <option>Option 1</option>
           <option>Option 2</option>
