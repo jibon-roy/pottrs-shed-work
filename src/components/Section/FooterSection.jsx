363ab7aa11df35bfd1469b1fac8c22fb039dfd5c
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-05 19:50:13 +0600

    footer section added

---
+++ src/components/Section/FooterSection.jsx
@@ -0,0 +1,11 @@
+const FooterSection = () => {
+  return (
+    <footer className="container px-2 md:px-10 py-5 lg:px-20 2xl:px-36 bg-primary-green">
+      <div className="relative">
+        <input type="text" className="h-16" />
+      </div>
+    </footer>
+  );
+};
+
+export default FooterSection;
