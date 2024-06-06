b62529fb5b410332182378a222dc48238147e21d
Jibon Krishna Roy <mailme.jibon@gmail.com>
2024-06-06 23:51:34 +0600

    instagram update #2

--- src/components/pages/home/InstagramSection.jsx
+++ src/components/pages/home/InstagramSection.jsx
@@ -75,8 +75,8 @@ const InstagramSection = () => {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  viewBox="0 0 24 24"
                   strokeWidth={2}
+                  viewBox="0 0 24 24"
                   stroke="currentColor"
                   className="h-6 w-6"
                 >
